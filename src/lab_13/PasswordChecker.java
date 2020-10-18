ae64b550dcc7920054a8d862102a05ef2636cc18
Бабышев Максим <cmdazaza@gmail.com>
2020-10-18 11:42:08 +0300

    lab_13 refactored (separate conditions)

--- src/lab_13/PasswordChecker.java
+++ src/lab_13/PasswordChecker.java
@@ -8,14 +8,31 @@ public class PasswordChecker {
         while (true) {
             System.out.print("Введите пароль: ");
             pswd = scanner.next();
-            if (Pattern.matches("\\w{8,}", pswd) &&
-                    Pattern.matches(".*[A-Z]+.*", pswd) &&
-                    Pattern.matches(".*[a-z]+.*", pswd) &&
-                    Pattern.matches(".*\\d+.*", pswd))
-                System.out.println("Пароль ±прокатит");
-            else
+            if (pswd.equals("?")) {
                 System.out.println("Пароль должен состоять из 8 и более букв латиницы (обоих регистров!) и\n" +
                         "цифр, он может содержать '_'");
+                continue;
+            } else if (pswd.length() < 8) {
+                System.out.println("Пароль должен содержать минимум 8 символов");
+                continue;
+            } else if (!Pattern.matches("\\w+", pswd)) {
+                System.out.println("Пароль может состоять только из букв латиницы," +
+                        " цифр и знака '_'");
+                continue;
+            } else if (!Pattern.matches(".*[A-Z].*", pswd)) {
+                System.out.println("Пароль должен содержать минимум одну" +
+                        " заглавную букву");
+                continue;
+            } else if (!Pattern.matches(".*[a-z].*", pswd)) {
+                System.out.println("Пароль должен содержать минимум одну" +
+                        " строчную букву");
+                continue;
+            } else if (!Pattern.matches(".*\\d.*", pswd)) {
+                System.out.println("Пароль должен содержать минимум одну" +
+                        " цыфру");
+                continue;
+            }
+            System.out.println("Пароль ±прокатит");
         }
     }
 }
