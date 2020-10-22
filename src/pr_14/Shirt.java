01a4345b9d96a04145a841e949b666093048fa83
Бабышев Максим <cmdazaza@gmail.com>
2020-10-22 14:16:47 +0300

    pr_14 completed (Shirts)

---
+++ src/pr_14/Shirt.java
@@ -0,0 +1,25 @@
+package pr_14;
+
+public class Shirt {
+    String id;
+    String name;
+    String color;
+    String size;
+
+    Shirt(String id, String name, String color, String size) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.size = size;
+    }
+
+    public String toString() {
+        return String.format(
+                "Shirt(\n\t'id': '%s'," +
+                      "\n\t'type': '%s'," +
+                      "\n\t'color': '%s'," +
+                      "\n\t'size': '%s'\n)",
+                id, name, color, size);
+    }
+}
+
