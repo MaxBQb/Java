1e55959fb98b2242cbcfba60f7457c6f7fb1bac1
Бабышев Максим <cmdazaza@gmail.com>
2020-11-26 10:34:43 +0300

    pr_14 string refactor was incorrect

--- src/ru/mirea/pr_14/Main.java
+++ src/ru/mirea/pr_14/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("\t[Завершение ввода Ctrl-D]");
-        System.out.println("Введите строки Shirt[]:ru.mirea.pr_14");
+        System.out.println("Введите строки Shirt:");
         Scanner scanner = new Scanner(System.in);
         String[] dtmp;
         ArrayList<Shirt> arrayList = new ArrayList<>();
